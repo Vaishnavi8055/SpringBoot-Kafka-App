ef1674b2e2e086fe9dd3f0bca84c2b656f0e8578
Vaishnavi Chaurasia <vaishnavi.chaurasia@iot83.com>
2022-04-19 21:00:30 +0530

    Kafka Producer & Consumer Api using spring boot

---
+++ api/consumer/service/ConsumerService.java
@@ -0,0 +1,16 @@
+package com.project.kafka.api.consumer.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsumerService {
+
+    @KafkaListener(topics = "topic",
+            groupId = "group_id")
+
+    // Method
+    public void consume(String message) {
+        System.out.println("message = " + message);
+    }
+}
---
+++ api/producer/controller/ProducerController.java
@@ -0,0 +1,33 @@
+package com.project.kafka.api.producer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/kafka")
+public class ProducerController {
+
+    private static final String TOPIC_NAME = "topic";
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    // Publish messages using the GetMapping
+    @GetMapping("/publish/{message}")
+    public String publishMessage(@PathVariable("message")
+                                 final String message)
+    {
+
+        // Sending the message
+        kafkaTemplate.send(TOPIC_NAME, message);
+
+        return "Published Successfully !! Go & Check on Terminal";
+    }
+
+}
